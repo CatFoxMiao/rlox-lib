705cafa764864ca9e17dc6f511b67e9bd8962045
CatFoxMiao <kindonefox@qq.com>
2025-05-30 13:26:35 +0800

    add token

---
+++ src/token.rs
@@ -0,0 +1,67 @@
+#[derive(Debug, PartialEq, Clone)]
+pub enum Literal{
+    String(String),
+    Number(f64),
+    Bool(bool),
+    None
+}
+
+
+#[derive(Debug, PartialEq, Clone)]
+pub enum TokenType{
+    // Single-character tokens
+    LeftParen,
+    RightParen,
+    LeftBrace,
+    RightBrace,
+    Comma,
+    Dot,
+    Minus,
+    Plus,
+    Semicolon,
+    Slash,
+    Star,
+
+    // One or two chracter tokens
+    Bang,
+    BangEqual,
+    Equal,
+    EqualEqual,
+    Greater,
+    GreaterEqual,
+    Less,
+    LessEqual,
+
+    // Literals
+    Identifier,
+    String,
+    Number,
+
+    // Keywords
+    And,
+    Class,
+    Else,
+    False,
+    Fun,
+    For,
+    Nil,
+    If,
+    Print,
+    Or,
+    Return,
+    Super,
+    This,
+    True,
+    Var,
+    While,
+
+    Eof,    
+}
+
+#[derive(Debug, PartialEq, Clone)]
+pub struct Token{
+    pub token_type:TokenType,
+    pub lexeme:String,
+    pub literal:Literal,
+    pub line:usize
+}
